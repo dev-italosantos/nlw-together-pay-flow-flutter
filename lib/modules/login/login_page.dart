5cc909529976bc548606c8417ee3e6b17a62683a
Ítalo Santos <italotayllor@gmail.com>
2021-06-22 16:01:46 -0300

    nlw-together-modifying-login-page

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/social_login/social_login_button_widget.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -32,9 +34,35 @@ class _LoginPageState extends State<LoginPage> {
               child: Image.asset(
                 AppImages.person,
                 width: 208.0,
-                height: 373.0,
+                height: 300.0,
               ),
-            )
+            ),
+            Positioned(
+              bottom: size.height * 0.05,
+              left: 0,
+              right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 30.0, left: 70.0, right: 70.0),
+                    child: Text(
+                      "Organize seus boletos em um só lugar",
+                      textAlign: TextAlign.center,
+                      style: TextStyles.titleHome,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 30.0, left: 70.0, right: 70.0),
+                    child: SocialLoginButtonWidget(onTap: () {}),
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
