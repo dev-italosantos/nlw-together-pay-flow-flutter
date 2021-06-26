d01ae68c847d1fe2ef58d18315d5d04e90713d84
Ítalo Santos <italotayllor@gmail.com>
2021-06-25 21:47:54 -0300

    nlw-together-adicionando-controller-home

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,13 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
-class HomePage extends StatelessWidget {
+import 'home_controller.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+  final pages = [
+    Container(color: Colors.deepPurpleAccent),
+    Container(color: Colors.deepPurple),
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("HomePage"),
+      appBar: PreferredSize(
+        child: Container(
+          height: 152.0,
+          color: AppColors.primary,
+          child: ListTile(
+            title: Text.rich(
+              TextSpan(
+                text: "Olá, ",
+                style: TextStyles.titleRegular,
+                children: [
+                  TextSpan(
+                    text: "Italo Santos",
+                    style: TextStyles.titleRegular,
+                  )
+                ],
+              ),
+            ),
+            subtitle: Text(
+              "Mantenha suas contas em dia",
+              style: TextStyles.captionShape,
+            ),
+            trailing: Container(
+              height: 48.0,
+              width: 48.0,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+            ),
+          ),
+        ),
+        preferredSize: Size.fromHeight(152.0),
+      ),
+      body: pages[controller.currentPage],
+      bottomNavigationBar: Container(
+        height: 90.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {
+                controller.setPage(0);
+                setState(() {});
+              },
+              icon: Icon(Icons.home),
+              color: AppColors.primary,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 56.0,
+                width: 56.0,
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                controller.setPage(1);
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.description_outlined,
+                color: AppColors.body,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
