d59d05b3ea4d773f991944ca990a4a2720aa618c
Ítalo Santos <italotayllor@gmail.com>
2021-06-29 23:09:53 -0300

    nlw-together-boleto-page-add-instancia-controller

--- lib/modules/insert_boleto/insert_boleto_page.dart
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -1,9 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text2/flutter_masked_text2.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/input_text/input_text_widget.dart';
+import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
 
-class InsertBoletoPage extends StatelessWidget {
-  const InsertBoletoPage({Key? key}) : super(key: key);
+import 'insert_boleto_controller.dart';
+
+class InsertBoletoPage extends StatefulWidget {
+  final String? barcode;
+  const InsertBoletoPage({
+    Key? key,
+    this.barcode,
+  }) : super(key: key);
+
+  @override
+  _InsertBoletoPageState createState() => _InsertBoletoPageState();
+}
+
+class _InsertBoletoPageState extends State<InsertBoletoPage> {
+  final controller = insertBoleroController();
+
+  final moneyInputController = MoneyMaskedTextController(
+    leftSymbol: "R\$",
+    decimalSeparator: ",",
+  );
+  final dueDateInputTextContrller = MaskedTextController(mask: "00/00/0000");
+  final barcodeInputTextController = TextEditingController();
+
+  @override
+  void initState() {
+    if (widget.barcode != null) {
+      barcodeInputTextController.text = widget.barcode!;
+    }
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,21 +48,61 @@ class InsertBoletoPage extends StatelessWidget {
           color: AppColors.input,
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 93.0,
-              vertical: 24.0,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 93.0,
+              ),
+              child: Text(
+                "Preencha os dados do boleto",
+                style: TextStyles.titleBoldHeading,
+                textAlign: TextAlign.center,
+              ),
             ),
-            child: Text(
-              "Preencha os dados do boleto",
-              style: TextStyles.titleBoldHeading,
-              textAlign: TextAlign.center,
+            SizedBox(height: 24.0),
+            InputTextWidget(
+              label: "Nome do boleto",
+              icon: Icons.description_outlined,
+              onChanged: (value) {},
+              validator: controller.validateName,
             ),
-          ),
-        ],
+            InputTextWidget(
+              controller: dueDateInputTextContrller,
+              label: "Vencimento",
+              icon: FontAwesomeIcons.timesCircle,
+              onChanged: (value) {},
+              validator: controller.validateVencimento,
+            ),
+            InputTextWidget(
+              controller: moneyInputController,
+              label: "Valor",
+              icon: FontAwesomeIcons.wallet,
+              onChanged: (value) {},
+              validator: (_) =>
+                  controller.validateValor(moneyInputController.numberValue),
+            ),
+            InputTextWidget(
+              controller: barcodeInputTextController,
+              label: "Código",
+              icon: FontAwesomeIcons.barcode,
+              onChanged: (value) {},
+              validator: controller.validateCodigo,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: SetLabelButtons(
+        enableSecondaryColor: true,
+        primaryLabel: "Cancelar",
+        primaryOnPressed: () {
+          Navigator.pop(context);
+        },
+        secondaryLabel: "Cadastrar",
+        secondaryOnPressed: () {},
       ),
     );
   }
