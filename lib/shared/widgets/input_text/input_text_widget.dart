136786416075f407604c7776ecf4072add5da3d4
Ítalo Santos <italotayllor@gmail.com>
2021-07-07 21:46:27 -0300

    nlw-together-add-animatedCard-input-widget

--- lib/shared/widgets/input_text/input_text_widget.dart
+++ lib/shared/widgets/input_text/input_text_widget.dart
@@ -1,3 +1,4 @@
+import 'package:animated_card/animated_card.dart';
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -22,46 +23,49 @@ class InputTextWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 16.0),
-      child: Column(
-        children: [
-          TextFormField(
-            controller: controller,
-            onChanged: onChanged,
-            initialValue: initialValue,
-            validator: validator,
-            style: TextStyles.input,
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.zero,
-              labelText: label,
-              labelStyle: TextStyles.input,
-              icon: Row(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 18.0),
-                    child: Icon(
-                      Icons.ac_unit,
-                      color: AppColors.primary,
+    return AnimatedCard(
+      direction: AnimatedCardDirection.left,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 16.0),
+        child: Column(
+          children: [
+            TextFormField(
+              controller: controller,
+              onChanged: onChanged,
+              initialValue: initialValue,
+              validator: validator,
+              style: TextStyles.input,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.zero,
+                labelText: label,
+                labelStyle: TextStyles.input,
+                icon: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 18.0),
+                      child: Icon(
+                        Icons.ac_unit,
+                        color: AppColors.primary,
+                      ),
                     ),
-                  ),
-                  Container(
-                    width: 1,
-                    height: 48.0,
-                    color: AppColors.stroke,
-                  ),
-                ],
+                    Container(
+                      width: 1,
+                      height: 48.0,
+                      color: AppColors.stroke,
+                    ),
+                  ],
+                ),
+                border: InputBorder.none,
               ),
-              border: InputBorder.none,
             ),
-          ),
-          Divider(
-            height: 1,
-            thickness: 1,
-            color: AppColors.stroke,
-          ),
-        ],
+            Divider(
+              height: 1,
+              thickness: 1,
+              color: AppColors.stroke,
+            ),
+          ],
+        ),
       ),
     );
   }
